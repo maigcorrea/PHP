8375469fb4f878fc14ab8c3b32319bfad33aaa93
unknown <anamaitegarciacorrea@gmail.com>
2024-11-28 12:53:35 +0100

    Formulario de registro e inicio de sesión de una web

---
+++ POO/BasesDatos/28-11/formulario.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+                
+<!-- header(location) -->
+    <?php
+        if(isset($_POST["env"])){
+
+            if(strlen($_POST["pass"])>=8 && strlen($_POST["pass"])<=20){
+                if($_POST["pass"]===$_POST["pass2"]){
+                    $db=new mysqli('localhost','root','','ejercicios');
+                    
+                    if($res = $db->query('Select nombre from usuarios where nombre="'.$_POST["nom"].'";')){
+                        if($res->num_rows == 0){
+                            $db->query("Insert into usuarios values('".$_POST["nom"]."','".$_POST["pass"]."');");
+                            echo "Usuario insertado correctamente";
+                        }else{
+                            echo "El usuario ya existe";
+                            if(isset($_POST["env1"])){
+                                if($res=query('Select nombre from usuarios where nombre="'.$_POST["nom1"].'" and contrasenia="'.$_POST["pass1"].'";')){
+                                    if($res->num_rows == 1){
+                                        echo "Has iniciado sesión con exito";
+                                    }else{
+                                        echo "El nombre de usuario o la contraseña no coinciden";
+                                    }
+                                }
+                            }else{
+                                echo '
+                                <form action="#" method="post" enctype="multipart/form-data">
+                                    <label for="nom1">Introduce tu nombre</label><br>
+                                    <input type="text" name="nom1" id=""><br>
+                                    <label for="pass1">Introduce la contraseña:</label><br>
+                                    <input type="text" name="pass1" id=""><br>
+                                    
+    
+                                    <input type="submit" value="Enviar" name="env1">
+                                </form>
+                                ';
+                            }
+                            
+                        }
+                        
+                        
+                    }
+                }else{
+                    echo "Las contraseñas no son iguales";
+                }
+            }else{
+                echo "La contraseña debe tener entre 8 y 20 caracteres";
+            }
+
+
+            
+        }else{
+            echo '
+                <form action="#" method="post" enctype="multipart/form-data">
+                    <label for="nom">Introduce tu nombre</label><br>
+                    <input type="text" name="nom" id=""><br>
+                    <label for="pass">Introduce la contraseña:</label><br>
+                    <input type="text" name="pass" id=""><br>
+                    <label for="pass2">Introduce la contraseña de nuevo:</label><br>
+                    <input type="text" name="pass2" id=""><br>
+
+                    <input type="submit" value="Enviar" name="env">
+                </form>
+            ';
+        }
+    ?>
+</body>
+</html>
