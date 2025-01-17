592da9ac9de4454b7c9b5c39956870aeface8993
unknown <anamaitegarciacorrea@gmail.com>
2025-01-17 10:11:53 +0100

    Escuela MVC

---
+++ MVC/escuela_MVC/class_bd.php
@@ -0,0 +1,15 @@
+<?php
+     require_once("../../../../cred.php");
+
+     class bd{
+        private $conn;
+
+        public function __construct(){
+            $this->conn=new mysqli("localhost",USU_CONN,PSW_CONN,"escuela");
+        } 
+
+        public function getConection(){
+            return $this->conn;
+        }
+     }
+?>
